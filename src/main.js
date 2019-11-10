6ed4cbc1c44ed860e1d0c5049c18b30393dba4ef
Lea <lea_chemoul@orange.fr>
2019-11-10 14:07:07 +0100

    :heavy_plus_sign: Socket support

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import VueRouter from "vue-router";
+import VueSocketIO from "vue-socket.io";
 
 import App from "./App.vue";
 import { state, getters, actions, mutations } from "./store";
@@ -13,9 +14,19 @@ Vue.use(Vuex);
 Vue.use(VueRouter);
 
 Vue.config.productionTip = false;
+const store = new Vuex.Store({ state, getters, actions, mutations });
+
+Vue.use(new VueSocketIO({
+	debug: true,
+	connection: "http://localhost:3001",
+	vuex: {
+		store,
+		actionPrefix: "SOCKET_",
+		mutationPrefix: "SOCKET_",
+	}}));
 
 new Vue({
-	store: new Vuex.Store({ state, getters, actions, mutations }),
+	store: store,
 	router: new VueRouter({ routes, mode: "history", hash: false }),
 	render: h => h(App),
 }).$mount("#app");
