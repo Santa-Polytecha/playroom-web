98541a436418b2b4e69f36928f11748f91b3fc84
Valentin Berger <Valentin.berger38@gmail.com>
2019-11-12 13:30:22 +0100

    :sparkles: Add `TextError`
    * Added `TextError` component. It display a message error below a field.
    * Add the component to `ConnectRoom`.

---
+++ src/components/TextError.vue
@@ -0,0 +1,30 @@
+<template>
+	<transition name="fade">
+		<p v-if="show" class="text-error text-danger">{{message}}</p>
+	</transition>
+</template>
+
+<script>
+export default {
+	name: "TextError",
+	props: {
+		message: {
+			type: String,
+			required: false,
+			default: "Error",
+		},
+		show: {
+			type: Boolean,
+			required: false,
+			default: false,
+		},
+	},
+};
+</script>
+
+<style scoped lang="scss">
+
+.text-error {
+	font-size: 10pt;
+}
+</style>
